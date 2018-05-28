dbf9331a8339dac3aaf76322ec16252cd96d8926
System Administrator <root@iMac-de-Rubson.local>
2018-05-28 11:44:57 -0300

    Implementado os métodos openLocalPdf() e downloadAndOpenPdf().

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DocumentViewer, DocumentViewerOptions } from '@ionic-native/document-viewer';
+import { FileTransfer } from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  platform: any;
+  constructor(public navCtrl: NavController, private document: DocumentViewer, private file: File, private transfer: FileTransfer) {
 
+  }
+
+  openLocalPdf() {
+    const options: DocumentViewerOptions = {
+      title: 'My PDF'
+    };
+
+    this.document.viewDocument('assets/PDF/pedfComprovantePagamentoContasGVT.pdf', 'application/pdf', {})
+  }
+
+  downloadAndOpenPdf() {
+
+    let path = null;
+
+    if(this.platform.is('ios')) {
+        path = this.file.documentsDirectory;
+    }else {
+      path = this.file.dataDirectory;
+    }
+
+    const transfer = this.transfer.create();
+    transfer.download('https://devdactic.com/html/5-simple-hacks-LBT.pdf', path + 'myfile.pdf').then(entry => { 
+
+      let url = entry.toURL();
+      this.document.viewDocument(url, 'application', {});
+    
+    });
+      
   }
 
 }
